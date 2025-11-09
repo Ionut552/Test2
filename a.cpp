9b8bfb9447e8df3b44cff87122ba777be8df19ea
Ionut <ipetre986@gmail.com>
2025-11-09 22:00:48 +0200

    Update mod 1000009

--- a.cpp
+++ a.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define NMAX 1005
 #define ll long long
+#define mod 1000009
 
 std::ifstream f("testgit.in");
 std::ofstream g("testgit.out");
@@ -12,6 +13,6 @@ ll ans;
 
 int main() {
     cin >> n;
-    for(int i=1;i<=n;i++)cin >> a, ans+=a;
+    for(int i=1;i<=n;i++)cin >> a, ans=(ans+a)%mod;
     cout << ans;
 }
