340c336a9805aaf62677b8ea4bfe71138879f1ff
Ionut <ipetre986@gmail.com>
2025-11-09 21:17:01 +0200

    first commit

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define NMAX 1005
+#define ll long long
+
+std::ifstream f("testgit.in");
+std::ofstream g("testgit.out");
+
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
